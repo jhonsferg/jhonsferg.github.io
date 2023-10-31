e4b6b8d9f60037c5cb7a1264108e14e70806139a
jhonsferg <jhon.fernandez.sg@gmail.com>
2023-10-31 08:21:26 -0500

    Settings for alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+    }
+  }
 })
